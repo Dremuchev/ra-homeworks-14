67d18662684c2e6b63ce5b1cda0f5b0f1624df29
Andrey <andrey.dremuchev@gmail.com>
2018-09-26 07:19:03 +0300

    -fix

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,40 +1,47 @@
 'use strict';
 
 function AuthForm({ onAuth }) {
-    const userData = {};
 
     const handleSubmit = event => {
+        const form = event.target.elements;
         event.preventDefault();
-        if (userData.name && userData.email && userData.password) {
+        const userData = { 
+            name: form.name.value, 
+            email: form.email.value,
+            password: form.password.value
+        };
+        if (onAuth) {
             onAuth(userData);
         }
     };
 
     const handleEvent = event => {
         const val = event.target.value;
-        userData[event.target.name] = event.target.value = event.target.name === 'email' 
+        event.target.value = event.target.name === 'email' 
             ? val.replace(/[^\w\d\-@._]/g, "")
             : val.replace(/[^\w\d_]/g, "");
     }
 
-    return (
-        <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={handleSubmit}>
-            <div className="Input">
-                <input required type="text" placeholder="Имя" onChange={() => (userData["name"] = event.target.value)} />
-                <label />
-            </div>
-            <div className="Input">
-                <input type="email" placeholder="Электронная почта" name="email" onChange={handleEvent} />
-                <label />
-            </div>
-            <div className="Input">
-                <input required type="password" placeholder="Пароль" name="password" onChange={handleEvent} />
-                <label />
-            </div>
-            <button type="submit">
-                <span>Войти</span>
-                <i className="fa fa-fw fa-chevron-right" />
-            </button>
-        </form>
-    );
+    return <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={handleSubmit}>
+        <div className="Input">
+          <input required type="text" placeholder="Имя" name="name" />
+          <label />
+        </div>
+        <div className="Input">
+          <input type="email" placeholder="Электронная почта" name="email" onChange={handleEvent} />
+          <label />
+        </div>
+        <div className="Input">
+          <input required type="password" placeholder="Пароль" name="password" onChange={handleEvent} />
+          <label />
+        </div>
+        <button type="submit">
+          <span>Войти</span>
+          <i className="fa fa-fw fa-chevron-right" />
+        </button>
+      </form>;
+}
+
+AuthForm.defaultProps = {
+    onAuth: console.log
 }
