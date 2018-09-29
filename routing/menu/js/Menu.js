d387b68b05d050fb96d3fc4ab86b39d981d8865f
Andrey <andrey.dremuchev@gmail.com>
2018-09-29 14:21:32 +0300

    -routing-done

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,10 @@
+const Menu = () => {
+    return (
+        <nav className="menu">
+            <NavLink exact activeClassName="menu__item-active" className="menu__item" to="/">Главная</NavLink>
+            <NavLink activeClassName="menu__item-active" className="menu__item" to="/drift">Дрифт-такси</NavLink>
+            <NavLink activeClassName="menu__item-active" className="menu__item" to="/timeattack">Time Attack</NavLink>
+            <NavLink activeClassName="menu__item-active" className="menu__item" to="/forza">Forza Karting</NavLink>
+        </nav>
+    )
+}
