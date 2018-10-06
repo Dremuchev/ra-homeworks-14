709e428d83ad16345e34c5af1c594a550a4e58ce
Andrey <andrey.dremuchev@gmail.com>
2018-10-06 13:27:19 +0300

    -lifecycle-done

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -1,19 +1,31 @@
 class SearchBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { fixed: false }
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = { fixed: false };
-  }
+    scrollHandler = () => {
+        this.setPosition(this.isFixed(window.pageYOffset))
+    }
 
-  render() {
-    return <SearchBoxView fixed={this.state.fixed} />
-  }
+    componentDidMount() {
+        document.addEventListener('scroll', this.scrollHandler)
+    }
 
-  isFixed() {
-    return undefined;
-  }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.scrollHandler)
+    }
 
-  setPosition() {
-    return undefined;
-  }
+    render() {
+        return <SearchBoxView fixed={this.state.fixed} />
+    }
+
+    isFixed(offset) {
+        const searchBox = document.querySelector('.search-box')
+        return offset > searchBox.offsetTop
+    }
+
+    setPosition(isFixed) {
+        this.setState({ fixed: isFixed })
+    }
 }
--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,9 +1,23 @@
 class Cart extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { isOpen: props.isOpen }
+    }
 
-  render() {
-    return (
-      <CartView {...this.props} />
-    );
-  }
+    componentWillReceiveProps(args) {
+        this.setState({ isOpen: args.items.length !== 0 })
+    }
 
+    shouldComponentUpdate(nextProps) {
+        return this.props.items.length !== nextProps.items.length
+    }
+
+    render() {
+        const { isOpen } = this.state
+        if (this.state.isOpen) {
+            return <CartView {...this.props} isOpen={isOpen} />
+        } else {
+            return null
+        }
+    }
 }
