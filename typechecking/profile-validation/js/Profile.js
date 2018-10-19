8140c5ff277234e8d7a6d54e998358b7266625dc
Andrey <andrey.dremuchev@gmail.com>
2018-10-19 20:34:21 +0300

    -fix-datinput-prop-types

--- typechecking/profile-validation/js/Profile.js
+++ typechecking/profile-validation/js/Profile.js
@@ -1,71 +1,83 @@
-'use strict';
+'use strict'
 
 const profileStyle = {
-  border: '1px solid #cccccc',
-  borderRadius: '5px',
-  width: '100%',
-  height: '100%',
-  margin: '5px'
-};
+    border: '1px solid #cccccc',
+    borderRadius: '5px',
+    width: '100%',
+    height: '100%',
+    margin: '5px',
+}
 
 const imageStyle = {
-  width: '200px',
-  height: '200px'
-};
+    width: '200px',
+    height: '200px',
+}
 
 const Profile = props => {
-  return (
-    <div className="col-md-4 text-center" style={{ marginBottom: '10px' }}>
-      <div style={profileStyle}>
-        <h2>{props.first_name} {props.last_name}</h2>
-        <div>
-          <img src={props.img} className="img-thumbnail" style={imageStyle} />
+    console.log('TCL: props', props)
+    return (
+        <div className="col-md-4 text-center" style={{ marginBottom: '10px' }}>
+            <div style={profileStyle}>
+                <h2>
+                    {props.first_name} {props.last_name}
+                </h2>
+                <div>
+                    <img
+                        src={props.img}
+                        className="img-thumbnail"
+                        style={imageStyle}
+                    />
+                </div>
+                <p>
+                    vk: <a href={props.url}>{props.url}</a>
+                </p>
+                <p>
+                    birthday: <a href={props.birthday}>{props.birthday}</a>
+                </p>
+            </div>
         </div>
-        <p>vk: <a href={props.url}>{props.url}</a></p>
-        <p>birthday: <a href={props.birthday}>{props.birthday}</a></p>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 const urlPropType = (props, propName, componentName) => {
-  const regex = /^https:\/\/vk\.com\/(id[0-9]+|[a-zA-Z0-9_-]+)$/;
-  let url = props[propName];
-  let isUrl = (typeof url === 'string') && regex.test(url);
-  if (!isUrl) {
-    return new Error(`Неверное значение '${props[propName]}' 
-    параметра ${propName} в компоненте ${componentName}`);
-  }
-  return null;
+    const regex = /^https:\/\/vk\.com\/(id[0-9]+|[a-zA-Z0-9_-]+)$/
+    let url = props[propName]
+    let isUrl = typeof url === 'string' && regex.test(url)
+    if (!isUrl) {
+        return new Error(`Неверное значение '${props[propName]}' 
+    параметра ${propName} в компоненте ${componentName}`)
+    }
+    return null
 }
 
 const birthdayPropType = (props, propName, componentName) => {
-  
-  const regex = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/;
-  const showError = () => new Error(`Неверное значение '${props[propName]}' 
-    параметра ${propName} в компоненте ${componentName}`);
-  let birthday = props[propName];
-  let isDate = (typeof birthday === 'string') && regex.test(birthday);
-  if (!birthday) {
-    return showError();
-  }
-  if (!isDate) {
-    return showError();
-  }
-  birthday = birthday.split('-');
-  birthday = new Date(birthday[0], birthday[1] - 1, birthday[2]);
-  if (birthday.getTime() >= Date.now()) {
-    return showError();
-  }
+    const regex = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/
+    const showError = () =>
+        new Error(`Неверное значение '${props[propName]}' 
+    параметра ${propName} в компоненте ${componentName}`)
+    let birthday = props[propName]
+    let isDate = typeof birthday === 'string' && regex.test(birthday)
+    if (!birthday) {
+        return showError()
+    }
+    if (!isDate) {
+        return showError()
+    }
+    let dateArr = birthday.split('-')
+    let dateStr = new Date(dateArr[0], dateArr[1] - 1, dateArr[2])
+    if (dateStr.getTime() >= Date.now()) {
+        return showError()
+    }
 }
 
 Profile.defaultProps = {
-  img: './images/profile.jpg'
+    img: './images/profile.jpg',
 }
 
 Profile.propTypes = {
-  first_name: PropTypes.string,
-  last_name: PropTypes.string,
-  url: urlPropType,
-  birthday: birthdayPropType
-};
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    img: PropTypes.string,
+    url: urlPropType,
+    birthday: birthdayPropType,
+}
